07a55af04bc60fe4ec62404488c3a32f955b046b
Gujarathi Shashank <gshasha@L-HYD00CJ9K8-M-2.local>
2020-11-04 17:47:25 +0530

    Ag Grid Changes

--- src/AgGridComponent/AgGrid.js
+++ src/AgGridComponent/AgGrid.js
@@ -12,6 +12,7 @@ class AgGrid extends Component {
 
         this.state = {
             columnDefs: [
+                {headerName: "Select", checkboxSelection: true},
                 {headerName: "Template Name", field: "templateName", sortable: true, filter: true},
                 {headerName: "Test Protocol", field: "testProtocol", sortable: true, filter: true},
                 {headerName: "Testing Status", field: "testingStatus", sortable: true, filter: true},
@@ -20,30 +21,48 @@ class AgGrid extends Component {
                 {headerName: "Status", field: "active", sortable: true, filter: true}
             ],
             rowData: []
-        }
+        };
+        this.onButtonClick = this.onButtonClick.bind(this)
     }
 
     componentDidMount() {
+        console.log("componentDidMount Invoked")
+        // Initially fetch api was here moved to ag grid life cycle method
+    }
+
+    onGridReady = (params) => {
+        this.gridApi = params.api;
+
         fetch("http://localhost:8080/color-templates/all")
             .then(res => res.json())
             .then(
                 (result) => {
                     this.setState({
-                        isLoaded: true,
                         rowData: result
                     });
                 }, (error) => {
                     this.setState({
-                        isLoaded: true,
                         error
                     });
                 })
     }
 
+    onButtonClick = () => {
+        const selectedNodes = this.gridApi.getSelectedNodes()
+        const selectedData = selectedNodes.map(node => node.data)
+        selectedData.map(d => console.log(d))
+        alert(selectedData.map(d => d.templateName + ' ' + d.testProtocol + ' ' + d.productCategory))
+        /*const selectedDataStringPresentation = selectedData.map( node => node.templateName + ' ' + node.model).join(', ')
+        alert(`Selected nodes: ${selectedDataStringPresentation}`)*/
+    };
+
+
     render() {
         return (
             <div className="ag-theme-alpine" style={{height: 600, width: 1400}}>
+                <button onClick={this.onButtonClick}>Get selected rows</button>
                 <AgGridReact columnDefs={this.state.columnDefs} rowData={this.state.rowData}
+                             onGridReady={this.onGridReady}
                              rowSelection="multiple" animateRows pagination={true} paginationAutoPageSize={true}
                 ></AgGridReact>
             </div>
